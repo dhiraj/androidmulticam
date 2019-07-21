4f02608a27fcaa3aadfc8027d145211e817f9424
Dhiraj Gupta <dhiraj@traversient.com>
2019-07-21 10:53:53 -0400

    Added and inited Timber for debug logging.

---
+++ app/src/main/java/com/dhirajgupta/multicam/App.kt
@@ -0,0 +1,14 @@
+package com.dhirajgupta.multicam
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        Timber.i("App starting up...")
+    }
+}
